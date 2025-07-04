5e37c4f004e03937517709be4dff1e5898d48073
kashiwagi-rena <azisai07@gmail.com>
2025-07-04 23:14:50 +0900

    チャレンジ問題の自分の回答

--- vue-calculator/src/App.vue
+++ vue-calculator/src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+  const message = "Vue";
+  const name = "rena";
+  const year = new Date().getFullYear();
 </script>
 
 <template>
   <main>
-    <h1>Hello, world!</h1>
+    <h1>Hello, {{ message }}!</h1>
   </main>
+  <footer>©️ {{ name }} {{ year }}</footer>
 </template>
 
 <style scoped>
@@ -12,5 +16,10 @@
     font-weight: bold;
     text-align: center;
     color: white;
+  }
+
+  footer {
+    color: white;
+    text-align: center;
   }
 </style>
